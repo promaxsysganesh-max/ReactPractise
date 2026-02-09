60f60368217071a2e6dc88602efc71b3ca5484f8
promaxsysganesh-max <promaxsys.ganesh@gmail.com>
2026-02-09 14:58:15 +0530

    simple counter UI using react hook

---
+++ counter/src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Counter.css"
+import { useState } from 'react'
+
+const Counter = () => {
+    const [count,setCount]=useState(0);
+  return (
+    <div className='counter-container'>
+      <p id='para'>You clicked button {count} times</p>
+      <button id='btn' onClick={()=>{
+        setCount(count+1)
+      }}>Click me</button>
+    </div>
+  )
+}
+
+export default Counter
