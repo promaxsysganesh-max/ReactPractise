3d130dfe8dbf0ffb02374c418bf887da59a7d1e3
promaxsysganesh-max <promaxsys.ganesh@gmail.com>
2026-02-20 15:36:09 +0530

    useContext passing data from parent to superchild

---
+++ useContext-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from 'react'
+import './App.css'
+import ChildA from './components/ChildA';
+
+const UserContext=createContext();
+
+function App() {
+  
+  const [user,setUser]=useState({name:"Ganesh"})
+  return (
+    <>
+      <UserContext.Provider value={user}>
+        <ChildA/>
+      </UserContext.Provider>
+    </>
+  )
+}
+
+export default App
+export {UserContext}
