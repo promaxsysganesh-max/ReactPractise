6fd122a8ecf799f4e7b0c10d3efe198102850f90
promaxsysganesh-max <promaxsys.ganesh@gmail.com>
2026-02-21 14:36:01 +0530

    added ResizeComponent in previous code

--- useEffectAdvance/src/App.jsx
+++ useEffectAdvance/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import DataFetcher from "./components/DataFetcher";
 import LoggerComponent from "./components/LoggerComponent";
+import ResizeComponent from "./components/ResizeComponent";
 import TimerComponent from "./components/TimerComponent";
 
 function App() { 
   return (
     //<LoggerComponent/>
     //<TimerComponent/>
-    <DataFetcher/>
+    //<DataFetcher/>
+    <ResizeComponent/>
   );
 }
 
